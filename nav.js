a9ee409b8556e8e86d36674ea16121da04a8533b
edulanghassan <edulanghassan@gmail.com>
2024-08-21 12:50:51 +0100

    01.01 added children-tasks/ to nav

--- nav.js
+++ nav.js
@@ -1,5 +1,5 @@
 // Dynamically load the navigation-bar
-fetch('/nav.html')
+fetch('/children-tasks/nav.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('nav-placeholder').innerHTML = data;
